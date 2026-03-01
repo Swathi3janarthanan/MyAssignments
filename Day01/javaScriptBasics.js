db2073b56046b23efa75d06733f2e18b4d54569d
Swathi3janarthanan <swathi3janarthanan@gmail.com>
2026-03-01 22:07:04 +0530

    Add files via upload
    Week1-Day01

---
+++ Day01/javaScriptBasics.js
@@ -0,0 +1,28 @@
+//Primitive datatypes
+
+//String type
+var firstName ="Swathi"
+console.log(typeof(firstName))
+
+//String type
+var companyName = "abc"
+console.log(typeof(companyName))
+
+//Number type
+var mobileNumber = 123456789
+console.log(typeof(mobileNumber))
+
+//boolean type
+var isAutomation = true
+console.log(typeof(isAutomation))
+
+//Undefined type (not assigned)
+var hasPlaywright
+console.log(typeof(hasPlaywright))
+
+//Print the values
+console.log("The first name is :" +firstName)
+console.log("The company name is :" +companyName)
+console.log("The mobile Number is :" +mobileNumber)
+console.log("Automation status :" +isAutomation)
+console.log("Undefined datatype :" +hasPlaywright)
